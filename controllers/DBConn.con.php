03605c5c77863098e11054b7c3afe1928d34186c
NKumar-coder21 <62969623+NKumar-coder21@users.noreply.github.com>
2021-04-10 15:32:09 -0500

    Update DBConn.con.php
    For Heroku

--- controllers/DBConn.con.php
+++ controllers/DBConn.con.php
@@ -1,22 +1,22 @@
 <?php
-//  <!--Created by Nathaniel Kumar @ GCU 2021 -->
-//connect to the database
-$servername = "localhost";
-$DBuser = "root";
-$DBpass = "root";
-$DBname = "DBLock";
-$port = "8889";
-$DBsock = "/Applications/MAMP/tmp/mysql/mysql.sock";
-$success = mysqli_connect(
-    $servername,
-    $DBuser,
-    $DBpass,
-    $DBname,
-    $port,
-    $DBsock
-);
-if (!$success) {
-    die("Connection to DB failed: " . mysqli_connect_error());
-} else {
-    $connection = $success;
-}
+	//  <!--Created by Nathaniel Kumar @ GCU 2021 -->
+	//connect to the database
+	$servername = "lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	$DBuser = "xztyzzd0q3647sig";
+	$DBpass = "dpxjf2500bg7kc2x";
+	$DBname = "rry2dq2j64uk6ug1";
+	$DBport = "3306";
+	$success = mysqli_connect(
+	    $servername,
+	    $DBuser,
+	    $DBpass,
+	    $DBname,
+	    $DBport
+	);
+	if (!$success) {
+	    die("Connection to DB failed: " . mysqli_connect_error());
+	} else {
+	    $connection = $success;
+	}
+ 
+
